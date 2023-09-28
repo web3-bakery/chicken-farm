d3f53f8be5e34edb469e1f4488f6f542a73ee617
huhn511 <sehe89@gmail.com>
2023-09-28 16:13:53 +0200

    try another path

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.BASE_PATH || '',
-}
+  router: {
+    base: process.env.BASE_PATH || "",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
