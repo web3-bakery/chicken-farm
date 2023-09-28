007c99b04d8e526bcf44d644fc0d691ddade65a0
huhn511 <sehe89@gmail.com>
2023-09-28 16:23:45 +0200

    fix path

--- app/next.config.js
+++ app/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  router: {
-    base: process.env.BASE_PATH || "",
-  },
-};
+  basePath: process.env.BASE_PATH || '',
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
