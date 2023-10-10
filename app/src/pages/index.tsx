1e101c812cc3002e631bfc5011b6d1dfe0da6041
huhn511 <sehe89@gmail.com>
2023-10-10 12:49:03 +0200

    add favicon

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -16,10 +16,9 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>🐔 Chicken Farm</title>
+        <title>Chicken Farm</title>
         <meta name="description" content="Play the new web3 game!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Base>
         <Container maxWidth="md">
